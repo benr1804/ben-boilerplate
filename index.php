b830b114dea8e099c7a9901a142703a26d9e3ff7
benr1804 <benr1804@gmail.com>
2014-11-19 23:12:03 +0000

    Adds in files from HTML5BP and Bootstrap

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+	<?php include "includes/head-tags.php"; ?>
+</head>
+<body>
+<!--[if lt IE 8]>
+	<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+
+<?php include "includes/header.php"; ?>
+
+<main>
+	<div id="main" class="container">
+		<div class="col-xs-12">
+			<h1 style="text-align:center;">Website Starts Here</h1>
+		</div>
+	</div>
+</main>
+
+<?php include "includes/footer.php"; ?>
+
+<?php include "includes/footer-tags.php"; ?>
+</body>
+</html>
